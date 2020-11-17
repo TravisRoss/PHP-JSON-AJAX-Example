83568ee472e339a9646c4ae0f45a4000a4be8d63
Travis Ross <travis1690@hotmail.co.uk>
2020-11-17 21:01:35 +0000

    Update getOcean.js
    Fixed the call to the distance and the name parameters from the data object. Added in a basic error report.

--- geonamesExample/libs/js/getOcean.js
+++ geonamesExample/libs/js/getOcean.js
@@ -1,25 +1,21 @@
 	$('#btnRun').click(function() {
 
 		$.ajax({
-			//get the data
 			url: "libs/php/getOceanInfo.php",
 			type: 'POST',
 			dataType: 'json',
 			data: {		//object called 'data'
 				
-				latitude: $('#inputLat').val(),
-				longtitude: $('#inputLong').val()
+				lat: $('#inputLat').val(),
+				long: $('#inputLong').val()
 			},
 			
 			success: function(result) {
-
 				console.log(result);
 
-				//If the status of the data returned is “​ok​” then the data held in 
-				//result is written into the HTML of the table data cells using standard jQuery functions.
 				if (result.status.name == "ok") {
-					$('#txtDistance').html(result['data'][0]['distance']);
-					$('#txtName').html(result['data'][0]['name']);
+					$('#txtDistance').html(result['data']['distance']);
+					$('#txtName').html(result['data']['name']);
 				}
 			
 			},
